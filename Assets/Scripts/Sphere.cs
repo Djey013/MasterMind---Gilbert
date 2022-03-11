cec2a297c18bf8501fc56a1469e320017a0f7b4d
Djey013 <97234391+Djey013@users.noreply.github.com>
2022-03-11 14:14:33 +0100

    Consultation

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -8,16 +8,21 @@ public class Sphere : MonoBehaviour
     public Manager manager;
 
     public int Index = -1;
+   
     
     private void Start()
     {
         manager = FindObjectOfType<Manager>();
+        
     }
 
     public Color GetSphereColor()
     {
+        
         return GetComponent<MeshRenderer>().material.GetColor("_Color");
     }
+    
+  
 
     public void SetSphereColor(Color newColor)
     {
